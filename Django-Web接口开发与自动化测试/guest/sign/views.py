b0659a74beeb9133cc81228eaf8666b8b822104f
lichu <1371772034@qq.com>
2017-07-04 20:49:03 +0800

    lintcode and django together

--- Django-Web接口开发与自动化测试/guest/sign/views.py
+++ Django-Web接口开发与自动化测试/guest/sign/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
 def index(request):
 	return render(request,'index.html')
+
+def login_action(request):
+	if request.method == 'POST':
+		username = request.POST.get('username','')
+		password = request.POST.get('password','')
+		if username == 'admin' and password == 'admin123':
+			return HttpResponseRedirect('/event_manager/')
+		else:
+			return render(request,'index.html',{'error':'username or password erroe!'})
+
+def event_manager(request):
+	return render(request,"event_manager.html")
