c906bb440dd4355f53c25455dea64e97d2802ed4
lichu <1371772034@qq.com>
2017-07-04 23:07:29 +0800

    django

--- Django-Web接口开发与自动化测试/guest/sign/views.py
+++ Django-Web接口开发与自动化测试/guest/sign/views.py
@@ -10,9 +10,14 @@ def login_action(request):
 		username = request.POST.get('username','')
 		password = request.POST.get('password','')
 		if username == 'admin' and password == 'admin123':
-			return HttpResponseRedirect('/event_manager/')
+			response = HttpResponseRedirect('/event_manager/')
+			#response.set_cookie('user',username,3600)
+			request.session['user']=username
+			return response
 		else:
 			return render(request,'index.html',{'error':'username or password erroe!'})
 
 def event_manager(request):
-	return render(request,"event_manager.html")
+	#username = request.COOKIES.get('user','')
+	username = request.session.get('user','')
+	return render(request,"event_manager.html",{'user':username})
