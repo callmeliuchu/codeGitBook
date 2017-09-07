fe4202feef65af6b44f869b8d5686e06cb6301e1
lichu <1371772034@qq.com>
2017-09-08 07:58:34 +0800

    js

--- 碰撞粒子/MinPQ.js
+++ 碰撞粒子/MinPQ.js
@@ -11,29 +11,61 @@ function MinPQ(){
 	this.less = function(i,j){
 		return this.arr[i].priority<this.arr[j].priority;
 	}
+    
+    this.swap = function(i,j){
+       var tmp = this.arr[i];
+       this.arr[i] = this.arr[j];
+       this.arr[j] = tmp;
+    }
+    
+    this.swim = function(i){
+    	
+        while(i<1){
+        	 var k = Math.floor(i/2);
+	     	 if(this.less(k,i))break;
+	         this.swap(k,i);
+	         i = k;
+        }
+    }
+    this.sink = function(i){
+    	while(i*2<this.n){
+    		var k = i*2;
+    		if(k+1<=this.n && this.less(k+1,k))k++;
+    		if(this.less(i,k))break;
+    		this.swap(k,i);
+    		i = k;
+    	}
+    }
 
-	this.insert = function(event){
-        this._insert(event.time,event);
-	}
-	this._insert = function(priority,obj){
+	// this.insert = function(event){
+ //        this._insert(event.time,event);
+	// }
+	this.insert = function(priority,obj){
         var item_obj = new item(priority,obj);
         this.n++;
-        for(var i=1;i<)
+        this.arr[this.n]=item_obj;
+        this.swim(this.n);
+        console.log(this.arr);
 	}
 	this.delMin = function(){
-		
+		var item_obj = this.arr[1];
+		this.arr[1] = this.arr[this.n];
+		this.n--;
+		this.sink(1);
+		return item_obj.obj;
 	}
 	this.isEmpty = function(){
-		return this.arr.length==0;
+		return this.n==0;
 	}
 }
 
 
-// var s = [1,4,2,5,623,4];
-// var pq = new MinPQ();
-// for(var i=0;i<s.length;i++){
-// 	pq.insert(s[i],s[i]);
-// }
-// while(!pq.isEmpty()){
-// 	console.log(pq.delMin());
-// }
+var s = [1,4,2,5,623,4];
+console.log(s);
+var pq = new MinPQ();
+for(var i=0;i<s.length;i++){
+	pq.insert(s[i],s[i]);
+}
+while(!pq.isEmpty()){
+	console.log(pq.delMin());
+}
