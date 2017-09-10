b59ccbda68f6195a858db02dda23ecb962a91428
lichu <1371772034@qq.com>
2017-09-10 08:52:42 +0800

    python

--- 统计学习方法/第五章决策树/cart.py
+++ 统计学习方法/第五章决策树/cart.py
@@ -1,4 +1,12 @@
+import numpy as np
 
+def load_data_1():
+	data_set = [ [1,1,'yes'],
+	             [1,1,'yes'],
+	             [1,0,'no'],
+	             [0,1,'no'],
+	             [0,1,'no']]
+	return data_set
 
 
 def load_data(file_path):
@@ -11,7 +19,22 @@ def load_data(file_path):
 	return  ret_data
 
 
-file_path = 'data/ex00.txt'
-print(load_data(file_path))
+# data = load_data_1()
+# data = np.mat(data)
+# print(data)
+# print(np.mean(data[:,0]))
 
+
+
+file_path = 'data/ex00.txt'
+data = load_data(file_path)
+data = np.mat(data)
+print(np.mean(data[:,-1]))
+# print(data)
+# print(data[:,-1])
+# print(data[:,-1].T.tolist()[0])
+# print(data[:,-1])
+# print(np.var(data[:,-1]))
+# arr = np.nonzero(data[:,1]>0.0)
+# print(data[arr,:])
 
